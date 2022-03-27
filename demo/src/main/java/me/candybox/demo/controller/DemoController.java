3e52adcedaa04c5fcc54996781ee9ac94712fd50
xiaodong <822384810@qq.com>
2022-03-27 20:11:44 +0800

    feat:Add Swagger3

--- demo/src/main/java/me/candybox/demo/controller/DemoController.java
+++ demo/src/main/java/me/candybox/demo/controller/DemoController.java
@@ -7,25 +7,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+
 @RequestMapping("/api")
 @RestController
 @RefreshScope
+@Tag(name="demo-controller",description="demo接口")
 public class DemoController {
-
-
-    @GetMapping("/demo")
-    @ResponseBody
-    public String demo(){
-        return "get demo";
-    }
-
 
     @Value("${demo}")
     private String demoConfig;
 
+
+/* 
+	OpenAPI 3								注解位置
+@Tag(name = “接口类描述”)			    Controller 类上
+@Operation(summary =“接口方法描述”)		Controller 方法上
+@Parameters							   Controller 方法上
+@Parameter(description=“参数描述”)		Controller 方法上 @Parameters 里
+@Parameter(description=“参数描述”)		Controller 方法的参数上
+(@Parameter(hidden = true) 或 @Operation(hidden = true) 或 @Hidden)
+@Schema								   DTO类上
+@Schema								   DTO属性上
+
+参考：https://github.com/OAI/OpenAPI-Specification/blob/3.0.1/versions/3.0.1.md
+*/
+    
+
+    @Operation(summary ="demo get接口")
+    @GetMapping("/demo")
+    @ResponseBody
+    public String demo(@Parameter(description="demo 参数") String demo){
+        return demo;
+    }
+
+
+    @Operation(summary ="demoConfig get接口")
     @GetMapping("/demoConfig")
     @ResponseBody
     public String demoConfig(){
         return demoConfig;
     }
+
+
 }
